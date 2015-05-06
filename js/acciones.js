aa004635c7e6040b296525c847c7f8db0ee7908a
AL11F13 <yosemarvell01@gmail.com>
2015-05-06 14:19:26 -0500

    cinco
    nueva

--- js/acciones.js
+++ js/acciones.js
@@ -7,9 +7,24 @@ document.addEventListener("deviceready",function(){
   $('#btnbeep').on('tap', function(){
 	  navigator.notification.beep(1);
 	  }); //btnbeep
-	  $('#Izquierda').on('swipeleft',
-	  function(){
-		  alert("Barrio a la izquierda");
+	  $('#Izquierda').on('swipeleft',function(){
+		  alert ("Barrio a la izquierda");
+	  });
+		  
+		  $('#Derecha').on ('swiperight',	  function(){
+			  alert ("Barrio a la Derecha");
+		  });
+			  
+			  document.addEventListener("pause", function (){
+				  $('#listado').append("<p> se pauso </p>");
+			  });
+				  
+			  document.addEventListener("resume",function(){
+				  $('#listado').append("<p> se reinicio</p>");
+			  });
+			  
+			  $(window).on ('orientation',function(e){
+				  $('#listado').append("<p> orientacion: "+ e.orientacion + "</p>");
 	  });
 }); 
 });
